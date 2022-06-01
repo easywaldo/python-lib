31c77cc95a2b5582182eec2b374e1a69234b04b1
easywaldo <easywaldoji@gmail.com>
2022-06-01 23:08:00 +0900

    bisect sample

---
+++ 0014_bisect.py
@@ -0,0 +1,23 @@
+import bisect
+
+result = []
+
+for score in [33, 99, 77, 70, 89, 90, 100]:
+    pos = bisect.bisect([60, 70, 80, 90], score)
+    grade = 'FDCBA'[pos]
+    result.append(grade)
+    
+print(result)
+
+result = []
+
+for score in [33, 99, 77, 70, 89, 90, 100]:
+    pos = bisect.bisect_left([60, 70, 80, 90], score)
+    grade = 'FDCBA'[pos]
+    result.append(grade)
+
+print(result)
+
+a = [60, 70, 80, 90]
+bisect.insort(a, 85)
+print(a)
