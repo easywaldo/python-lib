8afc07a3f76f5cdad36b7bafbf0dfc200cdf3268
easywaldo <easywaldoji@gmail.com>
2022-06-01 23:07:43 +0900

    pprint sample

---
+++ 0013_pprint.py
@@ -0,0 +1,5 @@
+import pprint
+
+result = {'user_id': 'easywaldo', 'age': 42, 'address': 'Seoul', 'description': 'C#, Java, PHP, Python, Spring, .net core, Nest.js, Django, FastAPI, TypeScript, JPA, Spring Batch'}
+
+pprint.pprint(result)
