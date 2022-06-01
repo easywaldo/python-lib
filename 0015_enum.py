4b7a711cdca2267662b4905236d0b8183a88b4b0
easywaldo <easywaldoji@gmail.com>
2022-06-01 23:38:14 +0900

    IntEnum sample

---
+++ 0015_enum.py
@@ -0,0 +1,28 @@
+from calendar import FRIDAY, SATURDAY, SUNDAY, THURSDAY, TUESDAY, WEDNESDAY, weekday
+from datetime import date
+from enum import IntEnum
+
+class Week(IntEnum):
+    MONDAY = 1,
+    TUESDAY = 2,
+    WEDNESDAY = 3,
+    THURSDAY = 4,
+    FRIDAY = 5,
+    SATURDAY = 6,
+    SUNDAY = 7
+
+def get_menu(input_date):
+    menu = {
+        Week.MONDAY: "김치찌개",
+        Week.TUESDAY: "비빔밥",
+        Week.WEDNESDAY: "된장찌개",
+        Week.THURSDAY: "불고기",
+        Week.FRIDAY: "갈비탕",
+        Week.SATURDAY: "라면",
+        Week.SUNDAY: "건빵",
+    }
+    print(input_date.isoweekday())
+    return menu[input_date.isoweekday()]
+
+print(get_menu(date(2022, 6, 1)))
+print(get_menu(date(2022, 6, 7)))
