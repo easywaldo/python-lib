8c74c6709f91383ec623b0909028ada063e0212f
easywaldo <easywaldoji@gmail.com>
2022-06-05 10:41:19 +0900

    itertools repeat and cycle sample

--- 0023_itertools_cycle.py
+++ 0023_itertools_cycle.py
@@ -1,5 +1,25 @@
 import itertools
 employee_list = itertools.cycle(['waldo', 'jenny', 'evan', 'john', 'emma'])
 
-while True:
-    print(next(employee_list))
+# while True:
+#     print(next(employee_list))
+
+
+def repeat(object, times=None):
+    # repeat(10, 3) --> 10 10 10
+    if times is None:
+        while True:
+            print('a')
+            print(object)
+            yield object
+    else:
+        for i in range(times):
+            print('b')
+            yield object
+result = list(map(pow, range(10), repeat(2)))
+print(result)
+
+
+
+result = list(map(pow, range(10), itertools.repeat(2)))
+print(result)
