7c4b2fc19e92ca90dba48b9bb2a1195efe7f006e
easywaldo <easywaldoji@gmail.com>
2022-06-06 13:54:38 +0900

    itertools combinations sample

---
+++ 0028_itertools_combinations.py
@@ -0,0 +1,12 @@
+import itertools
+
+it = itertools.combinations(range(1, 46), 6)
+print(it)
+
+# for num in it:
+#     print(num)
+
+print(len(list(it)))
+
+
+print(len(list(itertools.combinations_with_replacement(range(1, 46), 6))))
