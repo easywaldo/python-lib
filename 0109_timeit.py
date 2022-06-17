60d8b43af29d66e0de68b5e248b5660279d47ab4
easywaldo <easywaldoji@gmail.com>
2022-06-18 00:02:18 +0900

    timeit 를 이용한 함수실행시간 비교

---
+++ 0109_timeit.py
@@ -0,0 +1,14 @@
+import timeit
+
+def a():
+    result = []
+    for i in range(10000):
+        result.append(i)
+    return result
+
+def b():
+    return [i for i in range(10000)]
+
+if __name__ == '__main__':
+    print(timeit.timeit("a()", number=100, globals=globals()))
+    print(timeit.timeit("b()", number=100, globals=globals()))
