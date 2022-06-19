798139aa263545c5358e8654ca30b49dc5aee8d8
easywaldo <easywaldoji@gmail.com>
2022-06-19 23:29:55 +0900

    turtle sample color change feature

--- 0103_turtle.py
+++ 0103_turtle.py
@@ -8,6 +8,16 @@ t.left(120)
 t.clear()
 t.home()
 t.shape("turtle")
-for i in range(3):
+
+import random
+color_list = ["red", "green", "blue", "yellow"]
+for i in range(300):
     t.forward(100)
     t.left(120)
+    t.right(120)
+    t.left(120)
+    t.back(100)
+    t.circle(120)
+    pick_color = random.randrange(0, 4)
+    
+    t.bgcolor(color_list[pick_color])
